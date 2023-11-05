2c81137cee4a2e67b7f6b9570d64121ade211879
Sylvester <sylvestermsmithiii@gmail.com>
2023-11-04 19:35:06 -0700

    img test 7

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
     
   return (
-    <main className="h-full">
+    <main className="h-5/6">
 
       <div className="relative background-container h-full bg-cover" style={{ backgroundSize: 'cover', backgroundPosition: 'center bottom' }}>
         <div className="absolute top-12 left-0 right-0 text-center">
