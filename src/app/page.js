75c4060c78f77f876f866a25094659afc6457915
Sylvester <sylvestermsmithiii@gmail.com>
2024-02-05 21:20:05 -0800

    change spacing

--- src/app/page.js
+++ src/app/page.js
@@ -2,23 +2,25 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex-1 bg-cover bg-center relative">
-      <div className="absolute inset-0 overflow-hidden">
+    <main className="flex-1 flex flex-col my-8 mx-16">         
+      <div className="flex-1 mx-auto max-w-[900px]">
         <video
           autoPlay
           loop
           muted
           playsInline
-          className="w-full h-full object-cover"
+          className="aspect-auto border-black border-4 mt-8"
         >
           <source src="/fujw2816.MP4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-      {/* <div className="absolute top-12 left-0 right-0 text-center">
-        <h1 className="text-6xl">Taking Quality To New Heights</h1>
-      </div> */}
+      <div className="mx-auto my-auto text-xl flex flex-col justify-center">
+    
+          <h2 className="mb-4 text-center  text-2xl md:text-4xl  ">Call Now For A Quote!</h2>
+          <p className=" p-4 mb-4 text-center">Cell: 206-359-5016</p>
+      </div>
+      
     </main>
   );
 }
-
