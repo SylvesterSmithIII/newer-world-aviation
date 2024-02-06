eb26fbcbe123b4690204e100e9a548bb9395a74f
Sylvester <sylvestermsmithiii@gmail.com>
2024-02-05 21:26:33 -0800

    change spacing

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex-1 flex flex-col my-8 mx-16">         
+    <main className="flex-1 flex flex-col my-8 mx-2 md:mx-16">         
       <div className="flex-1 mx-auto max-w-[900px]">
         <video
           autoPlay
@@ -15,7 +15,7 @@ export default function Home() {
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="mx-auto my-auto text-xl flex flex-col justify-center">
+      <div className="mx-auto mb-56 md:mb-0 text-xl flex flex-col justify-center">
     
           <h2 className="mb-4 text-center  text-2xl md:text-4xl  ">Call Now For A Quote!</h2>
           <p className=" p-4 mb-4 text-center">Cell: 206-359-5016</p>
