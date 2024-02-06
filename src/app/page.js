3b96619acbc778d1596c9db372de50af64afef0a
Sylvester <sylvestermsmithiii@gmail.com>
2024-02-05 19:31:47 -0800

    changing

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,23 @@
-
+import Image from "next/image"
 
 export default function Home() {
-    
   return (
-    <main className="flex-1 bg-cover bg-center relative"
-    style={{
-      backgroundImage: 'url("/puyallup.JPG")',
-    }}
-    >
-
-        <div className="absolute top-12 left-0 right-0 text-center">
-          <h1 className="text-6xl">Taking Quality To New Heights</h1>
-        </div>
-
+    <main className="flex-1 bg-cover bg-center relative">
+      <div className="absolute inset-0 overflow-hidden">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/1yNoVJZKYq4?autoplay=1&controls=0&mute=1"
+          title="Embedded Video"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          className="w-full h-full object-cover"
+        />
+      </div>
+      {/* <div className="absolute top-12 left-0 right-0 text-center">
+        <h1 className="text-6xl">Taking Quality To New Heights</h1>
+      </div> */}
     </main>
-  )
+  );
 }
+
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -8,8 +8,10 @@ export default function Services() {
             <div>
                 <h2 className="my-8 text-center text-cyan-800 text-2xl md:text-4xl">Arial Photogrophy</h2>
                 <div className="flex flex-col md:flex-row gap-4 m-4 justify-around">
-                    <div className="mx-auto"><Image src={puyallup} alt="" width={600} height={600}  /></div>
-                    <div className="mx-auto"><Image src={puyallup} alt="" width={600} height={600} /></div>
+                    <div className="mx-auto w-[600px] h-[600px] text-center py-">
+                        Are you looking for a unique and captivating way to capture breathtaking aerial imagery? Look no further than our professional drone photography services. We combine cutting-edge drone technology with skilled pilots and photographers to deliver stunning visuals that will leave a lasting impression. Unlock the unlimited potential of aerial photography with our professional drone services. Elevate your visual storytelling and make a lasting impact with stunning aerial imagery. Contact us today to discuss your project and let us bring your vision to new heights!
+                    </div>
+                    <div className="mx-auto "><Image src={puyallup} alt="" width={600} height={600} className="border-black border-4 rounded-[10px]" /></div>
                 </div>
             </div>
 
